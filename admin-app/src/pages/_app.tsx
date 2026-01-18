3964a988f4d07d955deefb5a822f57c0972b4935
ayuloskov <yuloskov.aa@yandex.ru>
2026-01-18 16:29:14 +0700

    Template apps

---
+++ admin-app/src/pages/_app.tsx
@@ -0,0 +1,18 @@
+import type { AppType } from "next/app";
+import { Geist } from "next/font/google";
+
+import "~/styles/globals.css";
+
+const geist = Geist({
+	subsets: ["latin"],
+});
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+	return (
+		<div className={geist.className}>
+			<Component {...pageProps} />
+		</div>
+	);
+};
+
+export default MyApp;
